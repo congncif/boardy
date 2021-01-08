0bbe6f74c276f150ac2e27764337ff1176ac217c
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-01-08 09:14:47 +0700

    + Add TaskBoard completion

--- Boardy/ComponentKit/TaskBoard/TaskBoard.swift
+++ Boardy/ComponentKit/TaskBoard/TaskBoard.swift
@@ -9,18 +9,21 @@ import Foundation
 import UIKit
 
 public final class TaskBoard<Input, Output>: Board, GuaranteedBoard {
-    public typealias Completion = (Result<Output, Error>) -> Void
-    public typealias Executor = (NormalBoard, Input, @escaping Completion) -> Void
-    public typealias ErrorHandler = (NormalBoard, Error) -> Void
-    public typealias ProcessingHandler = (NormalBoard, Bool) -> Void
+    public typealias ExcutorCompletion = (Result<Output, Error>) -> Void
+    public typealias Executor = (NormalBoard, Input, @escaping ExcutorCompletion) -> Void
+
     public typealias SuccessHandler = (NormalBoard, Output) -> Void
+    public typealias ProcessingHandler = (NormalBoard, Bool) -> Void
+    public typealias ErrorHandler = (NormalBoard, Error) -> Void
+    public typealias CompletionHandler = (NormalBoard) -> Void
 
     public typealias InputType = Input
 
     private let executor: Executor
     private let successHandler: SuccessHandler
-    private let errorHandler: ErrorHandler
     private let processingHandler: ProcessingHandler
+    private let errorHandler: ErrorHandler
+    private let completionHandler: CompletionHandler
 
     public init(identifier: BoardID,
                 executor: @escaping Executor,
@@ -31,11 +34,13 @@ public final class TaskBoard<Input, Output>: Board, GuaranteedBoard {
                     alert.addAction(UIAlertAction(title: "Close", style: .cancel, handler: nil))
                     let viewController = $0.rootViewController.presentedViewController ?? $0.rootViewController
                     viewController.present(alert, animated: true)
-                }) {
+                },
+                completionHandler: @escaping CompletionHandler = { $0.complete() }) {
         self.executor = executor
-        self.errorHandler = errorHandler
-        self.processingHandler = processingHandler
         self.successHandler = successHandler
+        self.processingHandler = processingHandler
+        self.errorHandler = errorHandler
+        self.completionHandler = completionHandler
         super.init(identifier: identifier)
     }
 
@@ -44,6 +49,10 @@ public final class TaskBoard<Input, Output>: Board, GuaranteedBoard {
 
         execute(input: input) { [weak self] result in
             guard let self = self else { return }
+
+            defer {
+                self.completionHandler(self)
+            }
 
             self.processingHandler(self, false)
 
