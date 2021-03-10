f7220f26459b74f78ededdcf36da2e40dc04231a
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-03-10 10:47:43 +0700

    Add content watching ability to Board

--- Boardy/Core/Board/Board.swift
+++ Boardy/Core/Board/Board.swift
@@ -12,19 +12,72 @@ open class Board: IdentifiableBoard, OriginalBoard {
     public let identifier: BoardID
     public weak var delegate: BoardDelegate?
 
-    private weak var rootObject: AnyObject?
-
     public init(identifier: BoardID = .randomUnique()) {
         self.identifier = identifier
     }
 
+    /**
+     Give root object which associated with Board.
+     */
+
     public var root: AnyObject? { rootObject }
+
+    /**
+        Install Board into a root object. The root object will be set as an associated object with this Board. After that the Board can use some public functions which root object provided.
+
+         - Parameter rootObject: object Board will be linked to, should be `UIViewController`, `UIWindow` or a custom object.
+     */
 
     open func installIntoRoot(_ rootObject: AnyObject) {
         self.rootObject = rootObject
+    }
+
+    // MARK: Content watching
+
+    /**
+     Track content via its lifecycle. A reference will be created to content object to ensure that it will be still available or not at some time.
+
+     - Parameter content: object which should be watched.
+     */
+    public func watch(content: AnyObject) {
+        cleanBoxesIfNeeded()
+        contentBoxes.append(ContentBox(content: content))
+    }
+
+    /**
+     Give available status about contents of a Board. Use for checking the Board has active contents or not. Return `true` if has at least one watched content is still available, otherwise `false`. Required using with `watch(content:)`, if `watch(content:)` was not called before, this method always returns `false`.
+     */
+    public var watchedContentIsAvailable: Bool {
+        cleanBoxesIfNeeded()
+        return !contentBoxes.isEmpty
+    }
+
+    /**
+     Wouldn't like to watch old contents, reset at all.
+     */
+    public func resetWatchedContents() {
+        contentBoxes.removeAll()
+    }
+
+    // MARK: Private properties
+
+    private weak var rootObject: AnyObject?
+    private lazy var contentBoxes: [ContentBox] = []
+
+    // MARK: Private methods
+
+    private func cleanBoxesIfNeeded() {
+        contentBoxes.removeAll { !$0.isAvailable }
     }
 }
 
 extension Board: InstallableBoard {}
 
 extension Board: WindowInstallableBoard {}
+
+/// Box to keep weak reference to a content object which can be released at some time.
+struct ContentBox {
+    weak var content: AnyObject?
+
+    var isAvailable: Bool { content != nil }
+}
