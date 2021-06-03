b9a5eb3f83cd6b22e9ae85e7acbe4fa9615f6984
NGUYEN CHI CONG <congnc.if@gmail.com>
2021-06-03 21:55:50 +0700

    Enable to bind to bus when register Flow

--- Boardy/Core/BoardType/FLow+Variadic.swift
+++ Boardy/Core/BoardType/FLow+Variadic.swift
@@ -17,7 +17,18 @@ extension FlowManageable {
         nextHandler: @escaping (Target, Output) -> Void
     ) -> Self {
         let listIds: [FlowID] = matchedIdentifiers
-        return self.registerFlow(matchedIdentifiers: listIds, target: target, nextHandler: nextHandler)
+        return self.registerFlow(matchedIdentifiers: listIds, target: target, uniqueOutputType: uniqueOutputType, nextHandler: nextHandler)
+    }
+
+    @discardableResult
+    public func registerFlow<Output>(
+        matchedIdentifiers: FlowID...,
+        bindToBus bus: Bus<Output>
+    ) -> Self {
+        let listIds: [FlowID] = matchedIdentifiers
+        return self.registerFlow(matchedIdentifiers: listIds, target: bus, uniqueOutputType: Output.self, nextHandler: { target, data in
+            target.transport(input: data)
+        })
     }
 
     /// Guaranteed Flow ensures data must match with Output type if not handler will fatal in debug and will be skipped in release mode.
@@ -30,6 +41,17 @@ extension FlowManageable {
     ) -> Self {
         let listIds: [FlowID] = matchedIdentifiers
         return self.registerGuaranteedFlow(matchedIdentifiers: listIds, target: target, uniqueOutputType: uniqueOutputType, handler: handler)
+    }
+
+    @discardableResult
+    public func registerGuaranteedFlow<Output>(
+        matchedIdentifiers: FlowID...,
+        bindToBus bus: Bus<Output>
+    ) -> Self {
+        let listIds: [FlowID] = matchedIdentifiers
+        return self.registerGuaranteedFlow(matchedIdentifiers: listIds, target: bus, uniqueOutputType: Output.self, handler: { target, data in
+            target.transport(input: data)
+        })
     }
 
     /// Chain Flow handles step by step of chain of handlers until a handler in chain is executed. Eventually handler is mandatory to register this flow.
